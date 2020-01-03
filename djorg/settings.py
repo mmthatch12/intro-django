f27c9e9b660aa567f586d96402811b8013d9d9e3
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2020-01-03 16:31:44 -0700

    now have hidden key

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nx9wxo$w6k8-hp0-rg*+-d)_(4*v*d=j*($+^ywh3j%^epk1^%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- notes/models.py
+++ notes/models.py
@@ -6,5 +6,5 @@ from uuid import uuid4
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = modles.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
