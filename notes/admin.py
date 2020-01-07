5817728058ea8da5acd5a21e05f1d01dc07af38b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2020-01-07 09:36:22 -0700

    added subtitle

--- notes/admin.py
+++ notes/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Note, PersonalNote
 
-admin.site.register(Note)
-admin.site.register(PersonalNote)
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote, NoteAdmin)
 
--- notes/models.py
+++ notes/models.py
@@ -9,6 +9,8 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
+    # to add a new subtitle after the migration has been created you must add a default value or go blank=true
+    subtitle = models.CharField(max_length=200, default="If you don't know what to say")
     content = models.TextField(blank=True)
 
 class PersonalNote(Note):
